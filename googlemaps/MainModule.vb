5ccbf15047e345daf5495123e584a3cc02646bb8
算神 <lishewen@msn.com>
2019-03-25 08:33:29 +0800

    添加项目文件。

---
+++ googlemaps/MainModule.vb
@@ -0,0 +1,11 @@
+﻿Module MainModule
+    Const Path = "D:\Map\maptile\googlemaps\roadmap\"
+    Sub Main()
+        Dim FileList = My.Computer.FileSystem.GetFiles(Path, FileIO.SearchOption.SearchAllSubDirectories, "*.png")
+        For Each file In FileList
+            Console.WriteLine(file)
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
