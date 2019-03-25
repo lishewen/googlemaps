4c8e810dac1040b614c53fe4d93a2e7dddb882bb
算神 <lishewen@msn.com>
2019-03-25 10:14:44 +0800

    下载Google地图瓦片

---
+++ googlemaps/GoogleMapsContext.vb
@@ -0,0 +1,19 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.Data.Entity
+
+Public Class GoogleMapsContext
+    Inherits DbContext
+    Public Sub New()
+        Database.CreateIfNotExists()
+    End Sub
+
+    Public Property RoadMaps As DbSet(Of RoadMap)
+End Class
+
+Public Class RoadMap
+    <Key>
+    Public Property Id As Integer
+    Public Property X As Integer
+    Public Property Y As Integer
+    Public Property Z As Integer
+End Class
--- googlemaps/MainModule.vb
+++ googlemaps/MainModule.vb
@@ -1,11 +1,70 @@
-﻿Module MainModule
+﻿Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Net
+Imports System.Threading
+
+Module MainModule
     Const Path = "D:\Map\maptile\googlemaps\roadmap\"
+    Const Url = "http://mt1.google.cn/vt/lyrs=m@234000000&hl=zh-CN&gl=CN&src=app&x={0}&y={1}&z={2}"
+    Const FilePath = "D:\Map\maptile\googlemaps\roadmap\{0}\{1}\{2}.png"
     Sub Main()
-        Dim FileList = My.Computer.FileSystem.GetFiles(Path, FileIO.SearchOption.SearchAllSubDirectories, "*.png")
-        For Each file In FileList
-            Console.WriteLine(file)
-        Next
+        'GetFiles()
+        DownloadFile()
         Console.ReadLine()
     End Sub
 
+    Private Sub DownloadFile()
+        Using db As New GoogleMapsContext
+            Dim FileList = db.RoadMaps.ToList
+            For Each file In FileList
+                Dim MapUrl = String.Format(Url, file.X, file.Y, file.Z)
+                Dim filename = String.Format(FilePath, file.Z, file.X, file.Y)
+                'My.Computer.Network.DownloadFile(New Uri(MapUrl, UriKind.Absolute), filename, "", "", False, 100000, True)
+                Dim map = DownloadImage(MapUrl)
+                map.Save(filename, ImageFormat.Png)
+                'Thread.Sleep(3000)
+                Console.WriteLine(MapUrl)
+            Next
+        End Using
+        Console.WriteLine("下载完成")
+    End Sub
+
+    Private Sub GetFiles()
+        Dim FileList = My.Computer.FileSystem.GetFiles(Path, FileIO.SearchOption.SearchAllSubDirectories, "*.png")
+        Dim db As New GoogleMapsContext
+        For Each file In FileList
+            Console.WriteLine(file)
+            file = Replace(file, Path, "")
+            file = Replace(file, ".png", "")
+            Dim s = Split(file, "\")
+            db.RoadMaps.Add(New RoadMap With {
+            .Z = s(0),
+            .X = s(1),
+            .Y = s(2)
+            })
+        Next
+        db.SaveChanges()
+    End Sub
+
+    Private Function DownloadImage(ByVal url As String) As Bitmap
+        Dim bitmap As Bitmap = Nothing
+        Dim stream As Stream = DownloadResource(url)
+
+        If stream IsNot Nothing Then
+            bitmap = New Bitmap(stream)
+        End If
+
+        Return bitmap
+    End Function
+
+    Private Function DownloadResource(ByVal url As String) As Stream
+        Dim client As WebClient = New WebClient()
+        client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)")
+        Dim data As Byte() = client.DownloadData(url)
+        Dim stream As MemoryStream = New MemoryStream(data)
+        client.Dispose()
+
+        Return stream
+    End Function
 End Module
