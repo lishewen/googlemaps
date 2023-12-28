4d65403aea2476a36bf5b96eaf821eccdc845564
算神 <lishewen@msn.com>
2023-12-28 15:15:17 +0800

    更新google map瓦片采集地址

--- googlemaps/MainModule.vb
+++ googlemaps/MainModule.vb
@@ -6,7 +6,7 @@ Imports System.Threading
 
 Module MainModule
     Const Path = "D:\Map\maptile\googlemaps\roadmap\"
-    Const Url = "http://mt1.google.cn/vt/lyrs=m@234000000&hl=zh-CN&gl=CN&src=app&x={0}&y={1}&z={2}"
+    Const Url = "http://gac-geo.googlecnapps.cn/maps/vt?lyrs=m&x={0}&y={1}&z={2}"
     Const FilePath = "D:\Map\maptile\googlemaps\roadmap\{0}\{1}\{2}.png"
     Sub Main()
         'GetFiles()
@@ -59,10 +59,10 @@ Module MainModule
     End Function
 
     Private Function DownloadResource(ByVal url As String) As Stream
-        Dim client As WebClient = New WebClient()
+        Dim client As New WebClient()
         client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)")
         Dim data As Byte() = client.DownloadData(url)
-        Dim stream As MemoryStream = New MemoryStream(data)
+        Dim stream As New MemoryStream(data)
         client.Dispose()
 
         Return stream
